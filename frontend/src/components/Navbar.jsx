c16b7eb82f4c1ff4a0917b626706a9923f4a5003
Yanis Dillon <ygdillon@gmail.com>
2025-12-21 13:18:05 -0800

    Update workout icon to dumbbell icon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -75,9 +75,11 @@ function Navbar({ children }) {
 
   const WorkoutIcon = () => (
     <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-      <path d="M6.5 6.5h11v11h-11z" />
-      <path d="M6.5 6.5L12 12l5.5-5.5" />
-      <path d="M6.5 17.5L12 12l5.5 5.5" />
+      <path d="M14.4 14.4 9.6 9.6" />
+      <path d="M16 12h3a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2h-3" />
+      <path d="M8 12H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h3" />
+      <path d="M16 20h3a2 2 0 0 0 2-2v-5a2 2 0 0 0-2-2h-3" />
+      <path d="M8 20H5a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h3" />
     </svg>
   )
 
