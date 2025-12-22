abefeadaab918657e3a0f75c7d861e915ceeca91
Yanis Dillon <ygdillon@gmail.com>
2025-12-22 11:24:36 -0800

    Remove redundant tab titles from Nutrition, Schedule, and Workouts tabs

--- frontend/src/pages/ClientNutrition.jsx
+++ frontend/src/pages/ClientNutrition.jsx
@@ -68,7 +68,7 @@ function ClientNutrition({ clientId, clientName }) {
 
   return (
     <Container size="xl" py="xl">
-      <Title order={1} mb="xl">{isTrainerView ? 'Nutrition' : 'My Nutrition'}</Title>
+      {!isTrainerView && <Title order={1} mb="xl">My Nutrition</Title>}
 
       {nutritionGoals ? (
         <>
